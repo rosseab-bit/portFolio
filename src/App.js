3d3f65dcafad933d9d9a4040ffa7ef491bd0585e
rosseab <roseab.developer@gmail.com>
2023-08-18 18:46:17 -0300

    actualizo informacion personal

--- src/App.js
+++ src/App.js
@@ -1,91 +1,84 @@
-import './App.css';
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import NavButtons from './components/NavButtons.js';
-import BannerLogos from './components/BannerLogos.js';
-import AboutMy from './components/AboutMy';
-import CardSkils from './components/CardSkils';
-import MyContact from './components/MyContact';
-import MyProjects from './components/MyProjects';
-import Services from './components/Services';
-import MyNavBar from './components/MyNavBar';
-import { useMediaQuery } from '@mui/material';
-import NavBarMobile from './components/NavBarMobile';
-
+import "./App.css";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import NavButtons from "./components/NavButtons.js";
+import BannerLogos from "./components/BannerLogos.js";
+import AboutMy from "./components/AboutMy";
+import CardSkils from "./components/CardSkils";
+import MyContact from "./components/MyContact";
+import MyProjects from "./components/MyProjects";
+import Services from "./components/Services";
+import MyNavBar from "./components/MyNavBar";
+import { useMediaQuery } from "@mui/material";
+import NavBarMobile from "./components/NavBarMobile";
 
 function App() {
-	const switchMobile = useMediaQuery('(min-width:600px)')
-	const [aboutMy, setAboutMy] = React.useState(true);
-	const [mySkils, setMySkils] = React.useState(false);
-	const [myProyects, setMyProyects] = React.useState(false);
-	const [myServices, setMyServices] = React.useState(false);
-	const [myContact, setMyContact] = React.useState(false);
+  const switchMobile = useMediaQuery("(min-width:600px)");
+  const [aboutMy, setAboutMy] = React.useState(true);
+  const [mySkils, setMySkils] = React.useState(false);
+  const [myProyects, setMyProyects] = React.useState(false);
+  const [myServices, setMyServices] = React.useState(false);
+  const [myContact, setMyContact] = React.useState(false);
 
-	const switchFunctions = {
-		seeAboutMy: setAboutMy,
-		seeMySkils: setMySkils,
-		seeMyProyects: setMyProyects,
-		seeMyServices: setMyServices,
-		seeMyContact: setMyContact
-	}
+  const switchFunctions = {
+    seeAboutMy: setAboutMy,
+    seeMySkils: setMySkils,
+    seeMyProyects: setMyProyects,
+    seeMyServices: setMyServices,
+    seeMyContact: setMyContact,
+  };
 
+  const SwitchViewBody = () => {
+    if (aboutMy) {
+      return <AboutMy />;
+    } else if (mySkils) {
+      return <CardSkils />;
+    } else if (myContact) {
+      return <MyContact />;
+    } else if (myProyects) {
+      return <MyProjects />;
+    } else if (myServices) {
+      return <Services />;
+    }
+  };
 
-	const SwitchViewBody = () => {
-		if (aboutMy) {
-			return <AboutMy />
-		} else if (mySkils) {
-			return <CardSkils />
-		} else if (myContact) {
-			return (
-				<MyContact />
-			);
-		} else if (myProyects) {
-			return (
-				<MyProjects />
-			);
-		} else if (myServices){
-			return (
-				<Services />
-			);
-		}
-	}
-
-	return (
-		<>
-		{ switchMobile ? 
-		<Box sx={{ flexGrow: 1 }}>
-		<div style={{
-				'display': 'flex',
-				'flex-direction': 'row',
-				'justify-content': 'center'
-			}}
-			>
-				<NavButtons switchFunctions={switchFunctions} />
-			</div>
-	<Grid container spacing={2}>
-		<Grid item xs={12}
-			sx={{
-				display: 'flex',
-				flexDirection: 'row',
-				justifyContent: 'center',
-				marginTop: 5
-			}}
-		>
-			<BannerLogos />
-			
-		</Grid>
-		<Grid item xs={12} sx={{marginLeft: 20, marginRight: 20}}>
-			<SwitchViewBody />
-		</Grid>
-	</Grid>
-</Box>
-		:
-		<NavBarMobile /> 
-		}
-		
-		</>
-	);
+  return (
+    <>
+      {switchMobile ? (
+        <Box sx={{ flexGrow: 1 }}>
+          <div
+            style={{
+              display: "flex",
+              "flex-direction": "row",
+              "justify-content": "center",
+            }}
+          >
+            <NavButtons switchFunctions={switchFunctions} />
+          </div>
+          <Grid container spacing={2}>
+            <Grid
+              item
+              xs={12}
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                marginTop: 5,
+              }}
+            >
+              <BannerLogos />
+            </Grid>
+            <Grid item xs={12} sx={{ marginLeft: 20, marginRight: 20 }}>
+              <SwitchViewBody />
+            </Grid>
+          </Grid>
+        </Box>
+      ) : (
+        <NavBarMobile />
+      )}
+    </>
+  );
 }
 
 export default App;
--- src/components/AboutMy.js
+++ src/components/AboutMy.js
@@ -1,63 +1,89 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Avatar from '@mui/material/Avatar'; 
-import Stack from '@mui/material/Stack';
-import MyAvatar from '../static/img/my_avatar.jpg';
-import NavButtons from './NavButtons.js';
-import BannerLogos from './BannerLogos.js';
-import AboutPicture from '../static/img/about_my.jpg';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import MyAvatar from "../static/img/my_avatar.jpg";
+import NavButtons from "./NavButtons.js";
+import BannerLogos from "./BannerLogos.js";
+import AboutPicture from "../static/img/about_my.jpg";
+import "../App.css";
 
 function AboutMy() {
   return (
     <>
-	<div
-	    style={{
-
-	      'display': 'flex',
-	      'flex-direction': 'row',
-	      'justify-content': 'space-between',
-	      'margin-top': '50px',
-	    }}
-	  >
-	    <div style={{'margin-top': '130px'}}>
-	      <img src={AboutPicture} 
-		    width='400px' 
-		    height='auto' 
-		    border='2' 
-		    style={{'object-fit': 'cover', 
-			    'border-radius':'50%', 
-			    'height':'400px'}}
-	      />
-	    </div>
-	    <div style={{'margin-left':'30px', 'margin-right': '30px', 'border':'2', 'color': 'white'}}>
-	      <div style={{'margin-top': '30px', 'margin-left':'20px', 'margin-right':'10px'}}>
-	      <h1 >Sobre mi...</h1>
-	      <br/>
-	      <p style={{fontSize: 20}}>
-		Comence en el mundo de la informática desde joven por curiosidad. Inicié con un curso
-		de reparacion de computadoras en donde comprendí los conceptos generales del funcionamiento
-		de un computador. Luego de terminar el colegio secundario ingrese a cursar la carrera 
-		de profesor en Ciencias Físicas en la Universidad Nacional de Misiones, esta carrera me 
-		permitio ver todo un mundo nuevo. Por motivos personales la carrera no la termine, deje 
-		la misma sin cursar el ultimo año.<br/> <br/> 
-		Con todo esto nunca disminuyó mi interés por la informática. Conseguí mi primer
-		empleo en el área como SuportDesktop en una PyMES en la cual realizaba soporte a oficinas
-		pasando por todos los aspectos, desktop, notebooks, servers, imporesoras, redes, seguridad,
-		gestión de servidores de correos, etc.<br/> <br/>
-		En el camino me interesó la programación ya que realizaba pequeños scripts en Windows para 
-		automatizar pequeñas cosas en servidores. De este modo me fui metiendo al mundo del 
-		scripting y a la programación. Actualmente me encuentro trabajando exclusivamente en
-		servidores linux y programación desde hace aproximadamente 4 años. El lenguaje principal
-		con el que trabajo es Python, a parte de este tambien tengo experiencia con JavaScript.<br/> <br/>
-		En el 2022 comencé a estudiar para Analista de Sistemas en el Instituto IRSO. Actualmente me
-		me encuentro en 2do año de la carrera.
-	      </p>
-	      </div>
-	    </div>
-	  </div>
-	</>
- );
+      <div
+        style={{
+          display: "flex",
+          "flex-direction": "row",
+          "justify-content": "space-between",
+          "margin-top": "50px",
+        }}
+      >
+        <div style={{ "margin-top": "130px" }}>
+          <img
+            src={AboutPicture}
+            width="400px"
+            height="auto"
+            border="2"
+            style={{
+              "object-fit": "cover",
+              "border-radius": "50%",
+              height: "400px",
+            }}
+          />
+          <div className="containerMyName">
+            <h1 className="myName">
+              Ricardo Benitez
+            </h1>
+          </div>
+        </div>
+        <div
+          style={{
+            "margin-left": "30px",
+            "margin-right": "30px",
+            border: "2",
+            color: "white",
+          }}
+        >
+          <div
+            style={{
+              "margin-top": "30px",
+              "margin-left": "20px",
+              "margin-right": "10px",
+            }}
+          >
+            <h2>Sobre mi...</h2>
+            <br />
+            <p style={{ fontSize: 20 }}>
+              Comence en el mundo de la informática desde joven por curiosidad.
+              Inicié con un curso de reparacion de computadoras en donde
+              comprendí los conceptos generales del funcionamiento de un
+              computador. Luego de terminar el colegio secundario ingrese a
+              cursar la carrera de profesor en Ciencias Físicas en la
+              Universidad Nacional de Misiones, esta carrera me permitio ver
+              todo un mundo nuevo. Por motivos personales la carrera no la
+              termine, deje la misma sin cursar el ultimo año.
+              <br /> <br />
+              Con todo esto nunca disminuyó mi interés por la informática.
+              Conseguí mi primer empleo en el área como SuportDesktop en una
+              PyMES en la cual realizaba soporte a oficinas pasando por todos
+              los aspectos, desktop, notebooks, servers, imporesoras, redes,
+              seguridad, gestión de servidores de correos, etc.
+	      <br /> <br />
+	      Me desempeñe en roles como SysAdmin y DevOps. Realizando trabajos 
+	      tanto de opereaciones como de desarrollador. Actualmente me encuentro 
+	      trabajando como Desarrollador Full-Stack para Movistar Argentina.
+              <br /> <br />
+              En el 2022 comencé a estudiar para Analista de Sistemas en el
+              Instituto IRSO. En este momento me encuentro en 2do año de la
+              carrera.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AboutMy;
